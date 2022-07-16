f9a17e002689beec15d1b8bd37bb57cd7faeea7f
Zéfling <zefling@ikilote.net>
2022-07-17 01:51:06 +0200

    Admin : update user (fix)

--- src/Controller/ApiAdminUserUpdateController.php
+++ src/Controller/ApiAdminUserUpdateController.php
@@ -35,6 +35,8 @@ class ApiAdminUserUpdateController extends AbstractApiController implements Toke
             return $this->error(CodeError::USER_NO_PERMISSION, 'moderation role required', Response::HTTP_UNAUTHORIZED);
         }
 
+        $edit = false;
+
         // mapping
         $content = $request->toArray();
 
@@ -48,16 +50,17 @@ class ApiAdminUserUpdateController extends AbstractApiController implements Toke
         // username
 
         if (
-            empty($content['username'])  &&
+            isset($content['username']) &&
             !empty($username = trim($content['username'])) &&
-            $content['username'] !== $userEdit->get
+            $username !== $userEdit->getUsername()
         ) {
 
-            // test if login already exist
+            // test if username already exist
+            $userEmail =  $userRep->findOneBy(['username' => $username]);
 
-
-            if ($userEdit === null) {
+            if ($userEmail === null) {
                 $userEdit->setUsername($username);
+                $edit = true;
             } else {
                 return $this->error(CodeError::LOGIN_ALREADY_EXISTS, 'This username already exists.');
             }
@@ -65,13 +68,17 @@ class ApiAdminUserUpdateController extends AbstractApiController implements Toke
 
         // password
 
-        if (!empty($content['password'])) {
+        if (
+            isset($content['password']) &&
+            !empty($content['password'])
+        ) {
             if (strlen($password = trim($content['password'])) > 8) {
                 $hashedPassword = $passwordHasher->hashPassword(
                     $user,
                     $password
                 );
                 $userEdit->setPassword($hashedPassword);
+                $edit = true;
             } else {
                 return $this->error(CodeError::PASSWORD_MISSING, 'No password or valid password');
             }
@@ -79,27 +86,78 @@ class ApiAdminUserUpdateController extends AbstractApiController implements Toke
 
         // email
 
-        if ($content['email'] !== $userEdit->getEmail()) {
+        if (
+            isset($content['email']) &&
+            filter_var($email = trim($content['email']), FILTER_VALIDATE_EMAIL) &&
+            $email !== $userEdit->getEmail()
+        ) {
 
-            if (!empty($content['email']) && filter_var($email = trim($content['email']), FILTER_VALIDATE_EMAIL)) {
+            // test if email already exist
+            $userEmail =  $userRep->findOneBy(['email' => $email]);
 
-                // test if email already exist
-                $userEmail =  $userRep->findOneBy(['email' => $email]);
-
-                if ($userEmail === null) {
-                    $userEdit->setEmail($email);
-                } else {
-                    return $this->error(CodeError::EMAIL_ALREADY_EXISTS, 'This email already exists.');
-                }
+            if ($userEmail === null) {
+                $userEdit->setEmail($email);
+                $edit = true;
             } else {
-                return $this->error(CodeError::EMAIL_MISSING, 'No email or valid email');
+                return $this->error(CodeError::EMAIL_ALREADY_EXISTS, 'This email already exists.');
             }
         }
 
-        $entityManager = $doctrine->getManager();
-        $entityManager->persist($userEdit);
-        $entityManager->flush();
+        $roles = $userEdit->getRoles();
+        if (isset($content['moderator'])) {
+            if ($content['moderator'] === true && !in_array('ROLE_MODERATOR', $roles)) {
+                $roles[] = 'ROLE_MODERATOR';
+                $edit = true;
+            } else if ($content['moderator'] !== true && ($key = array_search('ROLE_MODERATOR', $roles)) !== false) {
+                unset($roles[$key]);
+                $edit = true;
+            }
+        }
 
-        return $this->OK();
+        if (isset($content['admin'])) {
+            if ($content['admin'] === true && !in_array('ROLE_ADMIN', $roles)) {
+                $roles[] = 'ROLE_ADMIN';
+                $edit = true;
+            } else if ($content['admin'] !== true && ($key = array_search('ROLE_ADMIN', $roles)) !== false) {
+                unset($roles[$key]);
+                $edit = true;
+            }
+        }
+
+        $isAdmin = in_array('ROLE_ADMIN', $roles);
+
+        if (isset($content['banned']) || $isAdmin) {
+            if (
+                isset($content['banned']) && $content['banned'] === true && !in_array('ROLE_BANNED', $roles) &&
+                !$isAdmin
+            ) {
+                $roles[] = 'ROLE_BANNED';
+                $edit = true;
+            } else if (
+                (isset($content['banned']) && $content['banned'] !== true || $isAdmin) &&
+                ($key = array_search('ROLE_BANNED', $roles)) !== false
+            ) {
+                unset($roles[$key]);
+                $edit = true;
+            }
+        }
+
+        $userEdit->setRoles($roles);
+
+        if ($edit) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($userEdit);
+            $entityManager->flush();
+        }
+
+        $userArray = $userEdit->toArray();
+
+        unset(
+            $userArray['password']
+        );
+
+        return $this->OK(
+            $userArray
+        );
     }
 }
