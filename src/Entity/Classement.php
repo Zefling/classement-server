0eb5afd3fd88355935ba043249bc496515dc6b6f
Zéfling <zefling@ikilote.net>
2022-09-09 22:04:43 +0200

    More categories

--- src/Entity/Classement.php
+++ src/Entity/Classement.php
@@ -11,13 +11,31 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 enum Category: string
 {
-    case Anime = 'anime';
-    case Game = 'game';
-    case VideoGame = 'video.game';
-    case BoardGame = 'board.game';
-    case Movie = 'movie';
-    case Series = 'series';
-    case Vehicle = 'vehicle';
+    case Animal = "animal";
+    case Anime = "anime";
+    case BoardGame = "board.game";
+    case Book = "book";
+    case Comics = "comics";
+    case Computer = "computer";
+    case Ecology = "ecology";
+    case Figure = "figure";
+    case Game = "game";
+    case Geography = "geography";
+    case History = "history";
+    case Language = "language";
+    case Manga = "manga";
+    case Movie = "movie";
+    case Music = "music";
+    case People = "people";
+    case Politics = "politics";
+    case Programming = "programming";
+    case Science = "science";
+    case Series = "series";
+    case Show = "show";
+    case Sport = "sport";
+    case Technology = "technology";
+    case Vehicle = "vehicle";
+    case VideoGame = "video.game";
     case Other = 'other';
 }
 
