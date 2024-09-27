0dba3b48c74a583817c2f6fb701ad820388ec158
Zéfling <zefling@ikilote.net>
2024-09-27 22:31:44 +0200

    Fix regex for schema

--- src/Controller/Schema/ClassementSchema.php
+++ src/Controller/Schema/ClassementSchema.php
@@ -43,7 +43,7 @@ class ClassementSchema
             }
           }
         },
-        "titleTextColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "titleTextColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "titleTextOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "itemWidth": { "type": "number", "minimum": 16, "maximum": 300, "multipleOf": 1 },
         "itemWidthAuto": { "type": "boolean" },
@@ -55,8 +55,8 @@ class ClassementSchema
         "itemPadding": { "type": "number", "minimum": 0, "maximum": 20, "multipleOf": 1 },
         "itemBorder": { "type": "number", "minimum": 0, "maximum": 20, "multipleOf": 1 },
         "itemMargin": { "type": "number", "minimum": 0, "maximum": 20, "multipleOf": 1 },
-        "itemBackgroundColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
-        "itemBorderColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "itemBackgroundColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
+        "itemBorderColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "itemBackgroundOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "itemBorderOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "itemTextMinLine": { "type": "number", "minimum": 0, "maximum": 10, "multipleOf": 1 },
@@ -78,24 +78,24 @@ class ClassementSchema
             "top-bubble"
           ]
         },
-        "itemTextBackgroundColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "itemTextBackgroundColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "itemTextBackgroundOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
-        "lineBackgroundColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
-        "lineBorderColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "lineBackgroundColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
+        "lineBorderColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "lineBackgroundOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "lineBorderOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
-        "imageBackgroundColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "imageBackgroundColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "imageBackgroundImage": { "enum": ["none", "custom", "sakura", "etoile", "ciel", "iceberg", "axis"] },
         "imageBackgroundCustom": {
           "type": "string",
-          "pattern": "|data:image\\/.*|https:\\/\\/api\\.classement\\.net\\/images\\/.*\\.webp"
+          "pattern": "^(|data:image\\/.*|https:\\/\\/api\\.classement\\.net\\/images\\/.*\\.webp)$"
         },
         "imageWidth": { "type": "number", "minimum": 100, "maximum": 4000, "multipleOf": 1 },
         "imageHeight": { "type": "number", "minimum": 100, "maximum": 4000, "multipleOf": 1 },
         "imageSize": { "enum": ["", "cover"] },
         "imagePosition": { "enum": ["", "center"] },
         "axisLineWidth": { "type": "number", "minimum": 0, "maximum": 12, "multipleOf": 1 },
-        "axisLineColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "axisLineColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "axisLineOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "axisArrowWidth": { "type": "number", "minimum": 0, "maximum": 50, "multipleOf": 1 },
         "nameWidth": { "type": "number" },
@@ -106,7 +106,7 @@ class ClassementSchema
         "borderSpacing": { "type": "number", "minimum": -1, "maximum": 20, "multipleOf": 1 },
         "borderSize": { "type": "number", "minimum": 0, "maximum": 20, "multipleOf": 1 },
         "groupLineSize": { "type": "number", "minimum": 0, "maximum": 50, "multipleOf": 1 },
-        "groupLineColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "groupLineColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "groupLineOpacity": { "type": "number", "minimum": 0, "maximum": 100, "multipleOf": 1 },
         "direction": { "enum": ["ltr", "rtl"] },
         "sizeX": { "type": "number", "minimum": 2, "maximum": 20, "multipleOf": 1 },
@@ -117,8 +117,8 @@ class ClassementSchema
       "type": "array",
       "properties": {
         "name": { "type": "string" },
-        "bgColor": { "type": "string" },
-        "txtColor": { "type": "string" },
+        "bgColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
+        "txtColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "list": {
           "type": "array",
           "items": {
@@ -134,7 +134,7 @@ class ClassementSchema
         "id": { "type": "string" },
         "url": {
           "type": "string",
-          "pattern": "|data:image\\/.*|https:\\/\\/api\\.classement\\.net\\/images\\/.*\\.webp"
+          "pattern": "^(|data:image\\/.*|https:\\/\\/api\\.classement\\.net\\/images\\/.*\\.webp)$"
         },
         "name": { "type": "string", "maxLength": 200 },
         "height": { "type": "number", "minimum": 0 },
@@ -145,8 +145,8 @@ class ClassementSchema
         "date": { "type": "number", "minimum": 0 },
         "title": { "type": "number", "minimum": 0 },
         "annotation": { "type": "string", "maxLength": 1000 },
-        "bgColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
-        "txtColor": { "type": "string", "pattern": "|#[0-9a-fA-F]{6}" },
+        "bgColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
+        "txtColor": { "type": "string", "pattern": "^(|#[0-9a-fA-F]{6})$" },
         "x": { "type": "number", "minimum": 0 },
         "y": { "type": "number", "minimum": 0 }
       },
